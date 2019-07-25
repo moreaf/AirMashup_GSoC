52369a901b7466bc0092904f448146d673d8acb6
Albert Morea <albert@MacBook-MacBook-Pro-de-Albert-2.local>
2019-07-25 10:56:04 +0200

    update 2nd review 25/07/19

--- SERVER/FinalServer.js
+++ SERVER/FinalServer.js
@@ -31,28 +31,28 @@ var charts = JSON.parse(fs.readFileSync('/Users/albert/Desktop/AirMashup_GSoC/SE
 app.get('/changeAirports/:id',function(req,response){
   var form = new FormData();
     form.append('kml', fs.createReadStream(airports[req.params.id].kml))
-    form.submit('http://192.168.86.117:8080/kml/manage/upload')
+    form.submit('http://10.33.34.116:4567/kml/manage/upload')
     response.send("done")
 })
 
 app.get('/changeSRoutes/:id',function(req,response){
   var form = new FormData();
     form.append('kml', fs.createReadStream(sroutes[req.params.id].kml))
-    form.submit('http://192.168.86.117:8080/kml/manage/upload')
+    form.submit('http://10.33.34.116:4567/kml/manage/upload')
     response.send("done")
 })
 
 app.get('/changeIntRoutes/:id',function(req,response){
   var form = new FormData();
     form.append('kml', fs.createReadStream(introutes[req.params.id].kml))
-    form.submit('http://192.168.86.117:8080/kml/manage/upload')
+    form.submit('http://10.33.34.116:4567/kml/manage/upload')
     response.send("done")
 })
 
 app.get('/changeCharts/:id',function(req,response){
   var form = new FormData();
     form.append('kml', fs.createReadStream(charts[req.params.id].kml))
-    form.submit('http://192.168.86.117:8080/kml/manage/upload')
+    form.submit('http://10.33.34.116:4567/kml/manage/upload')
     response.send("done")
 })
 
